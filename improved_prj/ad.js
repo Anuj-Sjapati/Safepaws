60dcb51853010bfc8dafe1724db2ce397526d9e9
Saaait <pradhansaait@gmail.com>
2024-10-22 12:56:37 +0545

    did some major changes of layout and nav bar

--- improved_prj/ad.js
+++ improved_prj/ad.js
@@ -1,20 +1,20 @@
-// Variables for image rotation
-const images = [
-    "6th.jpg",
-    "7th.jpg",
-    "8th.jpg",
-    // Add more image URLs as needed
-];
-let currentIndex = 0;
+// // Variables for image rotation
+// const images = [
+//     "6th.jpg",
+//     "7th.jpg",
+//     "8th.jpg",
+//     // Add more image URLs as needed
+// ];
+// let currentIndex = 0;
 
-// Function to change the displayed image
-function changeImage() {
-    currentIndex = (currentIndex + 1) % images.length; // Loop back to the first image
-    document.getElementById("petImage").src = images[currentIndex];
-}
+// // Function to change the displayed image
+// function changeImage() {
+//     currentIndex = (currentIndex + 1) % images.length; // Loop back to the first image
+//     document.getElementById("petImage").src = images[currentIndex];
+// }
 
-// Set interval for image rotation every 5 seconds
-setInterval(changeImage, 5000); // Change image every 5 seconds
+// // Set interval for image rotation every 5 seconds
+// setInterval(changeImage, 5000); // Change image every 5 seconds
 
 // Variables for testimonial rotation
 let currentTestimonial = 0;

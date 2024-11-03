19ce2a7cff985067cd56c718ea2675574e07a03c
Saaait <pradhansaait@gmail.com>
2024-11-03 15:32:48 +0545

    hashed pw removed

--- __MAIN/view/HTML/signup.php
+++ __MAIN/view/HTML/signup.php
@@ -5,14 +5,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $password = $_POST['password'];
 
     $sql = "INSERT INTO users (username, email, phone, password) VALUES (?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ssss", $username, $email, $phone, $password);
 
     if ($stmt->execute()) {
-        echo "Sign-up successful!";
+        echo "Sign-up successful!";     
     } else {
         echo "Error: " . $stmt->error;
     }

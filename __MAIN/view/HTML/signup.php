39bc9fc05e61a346a8b8aadbd7f58c94735b4075
Saaait <pradhansaait@gmail.com>
2025-01-08 21:53:11 +0545

    verification fixed

--- __MAIN/view/HTML/signup.php
+++ __MAIN/view/HTML/signup.php
@@ -14,6 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($stmt->execute()) {
         // set username and email in session
+        //verification works
         $_SESSION['username'] = $username;
         $_SESSION['email'] = $email;
         echo "Sign-up successful!";     
